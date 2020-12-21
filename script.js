5f80e85dee83fadbef1e33884c4ff88b6e6e1ce3
Kónya Márton <konyamarci52@protonmail.com>
2020-12-21 10:31:52 +0100

    improved things

--- script.js
+++ script.js
@@ -3,11 +3,22 @@ var audioFile = new Audio('assets/Zámbó Jimmy  - Még nem veszíthetek [ALPÁR
 audioFile.autoplay = true;
 audioFile.loop = true;
 
+function setup(){
+	if(audioFile.paused == true){
+		document.getElementById('paragraph').innerHTML = "Kattints a képre a jóságért!!44!!négy!! 😊";
+	}
+	else{
+		document.getElementById('paragraph').innerHTML = "Kattints a képre, hogy megálljon a zene"
+	}
+}
+
 function audioToggle(){
 	if(audioFile.paused == true){
 		audioFile.play();
+		setup();
 	}
 	else{
 		audioFile.pause();
+		setup();
 	}
 }
